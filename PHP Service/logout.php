3ce3bf6e2a33fd38c992641039f59fd516b82c88
Gavin1086 <34149012+Gavin1086@users.noreply.github.com>
2019-02-14 13:04:06 +0800

    Update logout.php

--- PHP Service/logout.php
+++ PHP Service/logout.php
@@ -1,7 +1,6 @@
 <?php
-   session_start();
-   
-   if(session_destroy()) {
-      header("Location: login.html");
-   }
+	session_start();
+	session_destroy();
+	$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+    header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 ?>
