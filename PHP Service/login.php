a5b70b2fc750f9ab6dc868849418d6de3ecc00dc
Gavin1086 <34149012+Gavin1086@users.noreply.github.com>
2019-02-14 12:54:02 +0800

    Update login.php
    Login function complete (Still need variable sync with frontend)

--- PHP Service/login.php
+++ PHP Service/login.php
@@ -1,1 +1,34 @@
-//login
+<?php
+	
+	require_once("db_config.php");
+	
+	session_start();
+
+	if(!isset($_SESSION["username"]))
+	{
+		$user_name = mysqli_real_escape_string($db, $_POST['username']);
+		
+		$user_password = mysqli_real_escape_string($db, $_POST['password']);
+		
+		$query = "SELECT * FROM sodexo-member WHERE username = '$user_name' and password = '$user_password'";
+		
+		$result = mysqli_query($db, $query);
+		
+		$count = mysqli_num_rows($result); 
+		
+		if($count == 1)
+		{
+			$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+			$_SESSION["username"] = $user_name;
+            setcookie("username", $user_name);
+			echo json_encode(array("state"=>"success", "username"=>$user_name));
+			$_SESSION['id'] = $row['id'];
+            $_SESSION['logged'] = true;
+		}
+		else
+		{
+			echo json_encode(array("state"=>"fail", "msg"=>"incorrect_information"));
+		}
+	}
+  	mysqli_close($db);
+?>
