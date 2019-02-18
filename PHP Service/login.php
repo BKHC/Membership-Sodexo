23fed24499b66a4538493d1019a1d2c895c80b8e
Gavin1086 <34149012+Gavin1086@users.noreply.github.com>
2019-02-18 23:39:39 +0800

    Update login.php

--- PHP Service/login.php
+++ PHP Service/login.php
@@ -6,11 +6,11 @@
 
 	if(!isset($_SESSION["username"]))
 	{
-		$user_name = mysqli_real_escape_string($db, $_POST['username']);
+		$email = mysqli_real_escape_string($db, $_POST['email']);
 		
 		$user_password = md5(mysqli_real_escape_string($db, $_POST['password']));
 		
-		$query = "SELECT * FROM users WHERE username = '$user_name' and password = '$user_password'";
+		$query = "SELECT * FROM users WHERE email = '$email' and password = '$user_password'";
 		
 		$result = mysqli_query($db, $query);
 		
@@ -19,9 +19,9 @@
 		if($count == 1)
 		{
 			$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-			$_SESSION["username"] = $user_name;
-			setcookie("username", $user_name);
-			echo json_encode(array("state"=>"success", "username"=>$user_name));
+			$_SESSION["email"] = $email;
+			setcookie("email", $email);
+			echo json_encode(array("state"=>"success", "email"=>$email));
 			$_SESSION['id'] = $row['id'];
 			$_SESSION['logged'] = true;
 		}
