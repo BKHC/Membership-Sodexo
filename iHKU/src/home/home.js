f46326ccf7e12c321162fe47b2cf5c2d2c97a5dd
fishying17 <fishying@hku.hk>
2019-03-05 10:48:52 +0800

    Update Hall function

--- iHKU/src/home/home.js
+++ iHKU/src/home/home.js
@@ -1,12 +1,134 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Alert, KeyboardAvoidingView, TouchableOpacity, Button, PixelRatio, Dimensions, TextInput, ImageBackground, Image, Platform, StyleSheet, Text, View} from 'react-native';
+import User from './user';
 
-export default class HomeScreen extends React.Component {
-  render(){
-    return(
-      <View>
-        <Text>HomeScreen</Text>
+// to normalize font size
+  const {
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
+  } = Dimensions.get('window');
+
+  // based on iphone 5s's scale
+  const scale = SCREEN_WIDTH / 320;
+
+  export function normalize(size) {
+    const newSize = size * scale
+    if (Platform.OS === 'ios') {
+      return Math.round(PixelRatio.roundToNearestPixel(newSize))
+    } else {
+      return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
+    }
+  }
+
+  export function getScreenWidth(){
+    return SCREEN_WIDTH
+  }
+  export function getScreenHeight(){
+    return SCREEN_HEIGHT
+  }
+
+class Comment extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderData(){
+    return this.props.items.map((item) =>
+    <View key = {item.id}>
+        <Text
+          style={{
+            fontSize:normalize(10),
+          }}
+        ><User User_ID={item.User_ID} /> - {item.date}</Text>
+        <Text
+          style={{
+            fontSize:normalize(24),
+          }}
+        >{item.topic}</Text>
+        <Text
+          style={{
+            fontSize:normalize(12),
+          }}
+        >運動: {item.rating_1}    文化: {item.rating_2}</Text>
+        <Text
+          style={{
+            fontSize:normalize(12),
+          }}
+        >環境: {item.rating_3}    仙制: {item.rating_4}</Text>
       </View>
     );
+  }
+
+  render(){
+    return (
+      <View>
+        {this.renderData()}
+      </View>
+      );
+
+
+  }
+}
+
+
+export default class HomeScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {items: [{id: 1, User_ID: 1, date: "5/3/2019", topic: "BTS is back", rating_1: 2, rating_2: 3,
+  rating_3: 4, rating_4: 5}]};
+    //this.items = [];
+  }
+
+  componentDidMount() {
+    const data = {Hall_ID: 0}
+    /***
+    fetch(`https://i.cs.hku.hk/~wyvying/test.php?Hall_ID=${encodeURIComponent(data.Hall_ID)}`, {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+    })
+    //.then(response => response.json()); // parses response to JSON
+      .then((data) => {
+        alert(JSON.stringify(data));
+        //this.item = JSON.parse(data);
+        this.setState({items: JSON.parse(data)});
+
+      }) // JSON-string from `response.json()` call
+      .catch(error => console.error(error));
+      ***/
+    }
+
+  render() {
+
+    if (this.state.items){
+    return (
+      <ImageBackground source={require('../../assets/loginBackground.jpg')} style={{width: '100%', height: '100%'}}>
+          <Text
+            style={{
+              height: 150
+            }}
+          >It is the top bar that I will do later
+          </Text>
+          <View
+            style={{
+              marginLeft:40,
+              fontFamily:'Helvetica Neue',
+              width:getScreenWidth()-80,
+            }}
+          >
+          <Comment items={this.state.items} />
+          </View>
+      </ImageBackground>
+    );
+  } else {
+  return (
+    <ImageBackground source={require('../../assets/loginBackground.jpg')} style={{width: '100%', height: '100%'}}>
+      <Text
+        style={{
+          height: 150
+        }}
+      >It is the top bar that I will do later
+      </Text>
+  </ImageBackground>);
+}
   }
 }
---
+++ iHKU/src/home/user.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class User extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {username: "default"};
+  }
+
+  componentDidMount(){
+    const data = {User_ID: this.props.User_ID};
+    /***
+    fetch(`https://i.cs.hku.hk/~wyvying/test.php?Hall_ID=${encodeURIComponent(data.User_ID)}`, {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+    })
+    //.then(response => response.json()); // parses response to JSON
+      .then((data) => {
+        alert(JSON.stringify(data));
+        this.setState({username: data)});
+
+      }) // JSON-string from `response.json()` call
+      .catch(error => console.error(error));
+      ***/
+  }
+
+  render(){
+    return(
+      <Text>{this.state.username}</Text>
+    );
+  }
+}
