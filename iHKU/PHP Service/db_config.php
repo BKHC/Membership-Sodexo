9d8fabb35a8d2218da986bc080a792152d0e1e56
Gavin1086 <34149012+Gavin1086@users.noreply.github.com>
2019-03-06 23:40:07 +0800

    Create db_config.php

---
+++ iHKU/PHP Service/db_config.php
@@ -0,0 +1,7 @@
+<?php
+	define('DB_SERVER', 'sophia.cs.hku.hk');
+	define('DB_USERNAME', 'ghwcheng');
+	define('DB_PASSWORD', 's2009181');
+	define('DB_DATABASE', 'ghwcheng');
+	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
