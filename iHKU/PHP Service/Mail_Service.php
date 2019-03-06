33ec3016c62a169a29258730a90c91f06f76ea8d
Gavin1086 <34149012+Gavin1086@users.noreply.github.com>
2019-03-06 23:47:41 +0800

    Create Mail_Service.php

---
+++ iHKU/PHP Service/Mail_Service.php
@@ -0,0 +1,17 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+	require_once("../library/PHPMailer/src/Exception.php");
+	require_once("../library/PHPMailer/src/PHPMailer.php");
+	require_once("../library/PHPMailer/src/SMTP.php");
+	$mail = new PHPMailer();                              // Passing `true`
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'user@example.com';                 // SMTP username
+    $mail->Password = 'secret';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+?>
