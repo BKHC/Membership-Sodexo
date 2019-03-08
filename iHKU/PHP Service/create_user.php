9645c53aa18c61934a466965fb2928e71689279e
Gavin1086 <34149012+Gavin1086@users.noreply.github.com>
2019-03-08 14:36:23 +0800

    Update create_user.php

--- iHKU/PHP Service/create_user.php
+++ iHKU/PHP Service/create_user.php
@@ -6,10 +6,8 @@ session_start();
 if (isset($_POST['user_reg'])) {
   $email = mysqli_real_escape_string($db, $_POST['email']);
   $password = mysqli_real_escape_string($db, $_POST['password']);
-  $first_name = mysqli_real_escape_string($db, $_POST['first_name']);
-  $last_name = mysqli_real_escape_string($db, $_POST['last_name']);
-  $phone_number = mysqli_real_escape_string($db, $_POST['phone_number']);
-  $user_check_query = "SELECT * FROM users WHERE email='$email' LIMIT 1";
+  $alias = mysqli_real_escape_string($db, $_POST['alias']);
+  $user_check_query = "SELECT * FROM User WHERE Email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
@@ -18,10 +16,11 @@ if (isset($_POST['user_reg'])) {
       echo json_encode("msg"=>"email already exists");
     }
   }
-  if (count($errors) == 0) {
+  else
+  {
   	$password = md5($password);
-  	$query = "INSERT INTO users (email, password, first_name, last_name, phone_number) 
-  			  VALUES('$email', '$password', '$first_name', '$last_name', '$phone_number')";
+  	$query = "INSERT INTO User (Email, Password, Alias) 
+  			  VALUES('$email', '$password', '$alias')";
   	mysqli_query($db, $query);
   	$_SESSION['email'] = $email;
   	$_SESSION['success'] = "You are now logged in";
