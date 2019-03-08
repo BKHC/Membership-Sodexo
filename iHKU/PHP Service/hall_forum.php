3ecd6dcc65f212ee43e583a84d6dd7e3bf035ecc
Gavin1086 <34149012+Gavin1086@users.noreply.github.com>
2019-03-08 17:27:14 +0800

    Create hall_forum.php

---
+++ iHKU/PHP Service/hall_forum.php
@@ -0,0 +1,20 @@
+<?php
+	
+require_once("db_config.php");
+session_start();
+  
+
+  $hall = mysqli_real_escape_string($db, $_POST['hall_id']);
+  $check_query = "SELECT * FROM Hall Rate WHERE HallID='$hall'";
+  $result = mysqli_query($db, $user_check_query);
+  $output = mysqli_fetch_assoc($result);
+  
+  if ($output) {
+      echo json_encode($output);
+    }
+
+else
+{
+  echo json_encode("msg"=>"There's no related response on this hall!")
+}
+?>
