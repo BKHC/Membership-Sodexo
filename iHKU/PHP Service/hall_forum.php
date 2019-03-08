2f9930bc8628db211605d5e9be6534b925cf0d91
fishying17 <fishying@hku.hk>
2019-03-08 19:28:57 +0800

    Update hall_forum.php
    Change from POST to GET

--- iHKU/PHP Service/hall_forum.php
+++ iHKU/PHP Service/hall_forum.php
@@ -1,14 +1,14 @@
 <?php
-	
+
 require_once("db_config.php");
 session_start();
-  
 
-  $hall = mysqli_real_escape_string($db, $_POST['hall_id']);
+
+  $hall = mysqli_real_escape_string($db, $_GET['hall_id']);
   $check_query = "SELECT * FROM Hall Rate WHERE HallID='$hall'";
   $result = mysqli_query($db, $user_check_query);
   $output = mysqli_fetch_assoc($result);
-  
+
   if ($output) {
       echo json_encode($output);
     }
