07135083bea0ca3e0b244e21cffedf8e72c7b93f
Gavin1086 <34149012+Gavin1086@users.noreply.github.com>
2019-03-27 23:25:30 +0800

    Update get_hall.php

--- iHKU/PHP Service/get_hall.php
+++ iHKU/PHP Service/get_hall.php
@@ -6,45 +6,33 @@
 		$query = "SELECT name FROM Hall";
 		
 		$result = mysqli_query($db, $query);
-		
-		$Hall_1_query_1 = "Select AVG(Rating_1) FROM Hall Rate WHERE HallID = 1";
-		
-		$Hall_1_result_1 = mysqli_query($db, $Hall_1_query_1);
 
-		$Hall_1_query_2 = "Select AVG(Rating_2) FROM Hall Rate WHERE HallID = 1";
-		
-		$Hall_1_result_2 = mysqli_query($db, $Hall_1_query_2);
-
-		$Hall_1_query_3 = "Select AVG(Rating_3) FROM Hall Rate WHERE HallID = 1";
-		
-		$Hall_1_result_3 = mysqli_query($db, $Hall_1_query_3);
-
-		$Hall_1_query_4 = "Select AVG(Rating_4) FROM Hall Rate WHERE HallID = 1";
-		
-		$Hall_1_result_4 = mysqli_query($db, $Hall_1_query_4);
-
-		$Hall_2_query_1 = "Select AVG(Rating_1) FROM Hall Rate WHERE HallID = 2";
-		
-		$Hall_2_result_1 = mysqli_query($db, $Hall_2_query_1);
-
-		$Hall_2_query_2 = "Select AVG(Rating_2) FROM Hall Rate WHERE HallID = 2";
-		
-		$Hall_2_result_2 = mysqli_query($db, $Hall_2_query_2);
-
-		$Hall_2_query_3 = "Select AVG(Rating_3) FROM Hall Rate WHERE HallID = 2";
-		
-		$Hall_2_result_3 = mysqli_query($db, $Hall_2_query_3);
-
-		$Hall_2_query_4 = "Select AVG(Rating_4) FROM Hall Rate WHERE HallID = 2";
-		
-		$Hall_2_result_4 = mysqli_query($db, $Hall_2_query_4);
-		
 		$json = array();
 
-		$row = mysqli_fetch_array($result);
-			
-		$json[]=array($row, $Hall_1_result_1, $Hall_1_result_2, $Hall_1_result_3, $Hall_1_result_4, $Hall_2_result_1, $Hall_2_result_2, $Hall_2_result_3, $Hall_2_result_4);
+
+	for($x = 0; $x < 10; $x++)
+	{
 		
+		$temp_query_1 = "SELECT AVG(`Rating_1`) FROM `Hall Rate` WHERE `HallID`= $x";
+		
+		$temp_result_1 = mysqli_query($db, $Hall_1_query_1);
+
+		$temp_query_2 = "SELECT AVG(`Rating_2`) FROM `Hall Rate` WHERE `HallID`= $x";
+		
+		$temp_result_2 = mysqli_query($db, $Hall_1_query_2);
+
+		$temp_query_3 = "SELECT AVG(`Rating_3`) FROM `Hall Rate` WHERE `HallID`= $x";
+		
+		$temp_result_3 = mysqli_query($db, $Hall_1_query_3);
+
+		$temp_query_4 = "SELECT AVG(`Rating_4`) FROM `Hall Rate` WHERE `HallID`= $x";
+		
+		$temp_result_4 = mysqli_query($db, $Hall_1_query_4);
+		
+		$json[$x]=array("ratiing_1"=>$temp_result_1, "rating_2"=>$temp_result_2, "rating_3"=>$temp_result_3, "rating_4"=>$temp_result_4);
+		
+	}
+
 		echo json_encode($json);
 
 
