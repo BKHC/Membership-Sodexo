a9507f2355e12d84f3af5cd1a616fb0ba1c5449a
Gavin1086 <34149012+Gavin1086@users.noreply.github.com>
2019-03-28 22:41:06 +0800

    Update post_comment.php

--- iHKU/PHP Service/post_comment.php
+++ iHKU/PHP Service/post_comment.php
@@ -3,17 +3,19 @@
 require_once("db_config.php");
 session_start();
 
-$UserID = mysqli_real_escape_string($db, $_POST['UserID']);
-$HallID = mysqli_real_escape_string($db, $_POST['HallID']);
-$Rating_1 = mysqli_real_escape_string($db, $_POST['Rating_1']);
-$Rating_2 = mysqli_real_escape_string($db, $_POST['Rating_2']);
-$Rating_3 = mysqli_real_escape_string($db, $_POST['Rating_3']);
-$Rating_4 = mysqli_real_escape_string($db, $_POST['Rating_4']);
-$Topic = mysqli_real_escape_string($db, $_POST['Topic']);
-$Comment = mysqli_real_escape_string($db, $_POST['Comment']);
-$Image_num = mysqli_real_escape_string($db, $_POST['Image_num']);
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+
+$UserID = mysqli_real_escape_string($db, $data["userID"]);
+$HallID = mysqli_real_escape_string($db, $data["hallID"]);
+$Rating_1 = mysqli_real_escape_string($db, $data["rating_1"]);
+$Rating_2 = mysqli_real_escape_string($db, $data["rating_2"]);
+$Rating_3 = mysqli_real_escape_string($db, $data["rating_3"]);
+$Rating_4 = mysqli_real_escape_string($db, $data["rating_4"]);
+$Topic = mysqli_real_escape_string($db, $data["topic"]);
+$Comment = mysqli_real_escape_string($db, $data["comment"]);
 
 
-$insert = "INSERT INTO `Hall Rate`(`UserID`, `HallID`, `Rating_1`, `Rating_2`, `Rating_3`, `Rating_4`, `Topic`, `Comment`, `Image_num`, `Date`) VALUES ($UserID, $HallID, $Rating_1, $Rating_2, $Rating_3, $Rating_4, $Topic, $Comment, $Image_num)";
+$insert = "INSERT INTO `Hall Rate`(`UserID`, `HallID`, `Rating_1`, `Rating_2`, `Rating_3`, `Rating_4`, `Topic`, `Comment`) VALUES ($UserID, $HallID, $Rating_1, $Rating_2, $Rating_3, $Rating_4, $Topic, $Comment)";
 
 ?>
