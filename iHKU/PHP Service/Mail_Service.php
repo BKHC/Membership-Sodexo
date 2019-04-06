c76c7b9d730cd694b256e4150afd69f2a4b2be4c
Gavin1086 <34149012+Gavin1086@users.noreply.github.com>
2019-04-06 14:32:02 +0800

    Update Mail_Service.php

--- iHKU/PHP Service/Mail_Service.php
+++ iHKU/PHP Service/Mail_Service.php
@@ -8,10 +8,10 @@
     //Server settings
     $mail->SMTPDebug = 2;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+    $mail->Host = 'mail.cs.hku.hk;mail.cs.hku.hk';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'user@example.com';                 // SMTP username
-    $mail->Password = 'secret';                           // SMTP password
+    $mail->Username = 'gavin97@hku.hk';                 // SMTP username
+    $mail->Password = 'hkus2009181';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 ?>
