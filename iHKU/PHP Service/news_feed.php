109bfac99701aa0b123423c55e7e708b1d40bac2
Gavin1086 <34149012+Gavin1086@users.noreply.github.com>
2019-04-21 00:43:22 +0800

    Still working, do not touch

--- iHKU/PHP Service/news_feed.php
+++ iHKU/PHP Service/news_feed.php
@@ -3,7 +3,20 @@
 require_once("../db_config.php");
 session_start();
 
-  $check_query = "SELECT * FROM `Hall Rate` ORDER BY Date DESC";
+  $user_id = $_POST['userID'];
+  $hist_query = "SELECT `keyword` FROM `User_search_hist` WHERE `UserID` = $user_id";
+  $hist_result = mysqli_query($db, $hist_query);
+  $count = mysqli_num_rows($result);
+  
+  if($count == 0)
+  {
+    //working on it
+  }
+
+  else
+  {
+
+  $check_query = "SELECT * FROM `Hall Rate` ORDER BY Date DESC UNION SELECT * FROM `Restaurant Rate` ORDER BY Date DESC UNION SELECT * FROM `Course Rate` ORDER BY Date DESC";
   $result = mysqli_query($db, $check_query);
   $json = array();
   $i = 0;
@@ -25,51 +38,8 @@ session_start();
       );
              $i = $i + 1;
  		}
+  }
     
-   
- 
-  $check_query1 = "SELECT * FROM `Restaurant Rate` ORDER BY Date DESC";
-  $result1 = mysqli_query($db, $check_query1);
-  
-  while($row=mysqli_fetch_array($result1))
- 		{
-      $id = (int)$row['UserID'];
-      $user_check_query = "SELECT Alias FROM User WHERE UserID=$id LIMIT 1";
-      $new_result = mysqli_query($db, $user_check_query);
-      $user = mysqli_fetch_assoc($new_result);
-      
- 			$json[$i] = array(
-        'id'=>$row['ID'],
-        'nickname'=>$user["Alias"],
-        'comment'=>$row['Comment'],
-        'date' => $row['Date'],
-        'like_num' => $row['Like_num'],
-        'dislike_num' => $row['Dislike_num']
-      );
-             $i = $i + 1;
- 		}
-    
-    
-  $check_query2 = "SELECT * FROM `Course Rate` ORDER BY Date DESC";
-  $result2 = mysqli_query($db, $check_query2);
-  
-  while($row=mysqli_fetch_array($result2))
- 		{
-      $id = (int)$row['UserID'];
-      $user_check_query = "SELECT Alias FROM User WHERE UserID=$id LIMIT 1";
-      $new_result = mysqli_query($db, $user_check_query);
-      $user = mysqli_fetch_assoc($new_result);
-      
- 			$json[$i] = array(
-        'id'=>$row['ID'],
-        'nickname'=>$user["Alias"],
-        'comment'=>$row['Comment'],
-        'date' => $row['Date'],
-        'like_num' => $row['Like_num'],
-        'dislike_num' => $row['Dislike_num']
-      );
-             $i = $i + 1;
- 		}
     
  		echo json_encode($json);
 ?>
