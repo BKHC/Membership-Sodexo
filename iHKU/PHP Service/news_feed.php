0cecc799a3aa583fdcac049bec026fc323666b8f
Gavin1086 <34149012+Gavin1086@users.noreply.github.com>
2019-04-22 00:53:11 +0800

    Update news_feed.php

--- iHKU/PHP Service/news_feed.php
+++ iHKU/PHP Service/news_feed.php
@@ -92,7 +92,7 @@ session_start();
   {
    $search_hist = mysqli_fetch_array($hist_result);
     
-  $check_query = "SELECT * FROM `Hall Rate` ORDER BY '$search_hist' DESC";
+  $check_query = "SELECT * FROM `Hall Rate` WHERE Topic LIKE '$search_hist'";
   $result = mysqli_query($db, $check_query);
   
   while($row=mysqli_fetch_array($result))
@@ -116,7 +116,7 @@ session_start();
              $i = $i + 1;
  		}
     
-    $check_query1 = "SELECT * FROM `Restaurant Rate` ORDER BY "$search_hist" DESC";
+    $check_query1 = "SELECT * FROM `Restaurant Rate` WHERE Topic LIKE '$search_hist'";
   $result1 = mysqli_query($db, $check_query1);
   
   while($row=mysqli_fetch_array($result1))
@@ -140,7 +140,7 @@ session_start();
              $i = $i + 1;
  		}
     
-    $check_query2 = "SELECT * FROM `Course Rate` ORDER BY '$search_hist' DESC";
+    $check_query2 = "SELECT * FROM `Course Rate` WHERE Topic LIKE '$search_hist'";
   $result2 = mysqli_query($db, $check_query2);
   
   while($row=mysqli_fetch_array($result2))
@@ -163,6 +163,79 @@ session_start();
       );
              $i = $i + 1;
  		}
+    
+    $check_query3 = "SELECT * FROM `Hall Rate` ORDER BY rand()";
+    $result3 = mysqli_query($db, $check_query3);
+  
+  while($row=mysqli_fetch_array($result3))
+ 		{
+      $id = (int)$row['UserID'];
+      $user_check_query = "SELECT Alias FROM User WHERE UserID=$id LIMIT 1";
+      $new_result = mysqli_query($db, $user_check_query);
+      $user = mysqli_fetch_assoc($new_result);
+      
+ 			$json[$i] = array(
+        'id'=>$row['ID'],
+        'nickname'=>$user["Alias"],
+        'Topic' =>$row['Topic'],
+        'rating_1' =>$row['Rating_1'],
+        'rating_2' =>$row['Rating_2'],
+        'rating_3' =>$row['Rating_3'],
+        'rating_4' =>$row['Rating_4'],
+        'comment'=>$row['Comment'],
+        'date' => $row['Date']
+      );
+             $i = $i + 1;
+ 		} 
+    
+    $check_query4 = "SELECT * FROM `Restaurant Rate` ORDER BY rand()";
+    $result4 = mysqli_query($db, $check_query4);
+  
+  while($row=mysqli_fetch_array($result4))
+ 		{
+      $id = (int)$row['UserID'];
+      $user_check_query = "SELECT Alias FROM User WHERE UserID=$id LIMIT 1";
+      $new_result = mysqli_query($db, $user_check_query);
+      $user = mysqli_fetch_assoc($new_result);
+      
+ 			$json[$i] = array(
+        'id'=>$row['ID'],
+        'nickname'=>$user["Alias"],
+        'Topic' =>$row['Topic'],
+        'rating_1' =>$row['Rating_1'],
+        'rating_2' =>$row['Rating_2'],
+        'rating_3' =>$row['Rating_3'],
+        'rating_4' =>$row['Rating_4'],
+        'comment'=>$row['Comment'],
+        'date' => $row['Date']
+      );
+             $i = $i + 1;
+ 		} 
+    
+    $check_query5 = "SELECT * FROM `Course Rate` ORDER BY rand()";
+    $result5 = mysqli_query($db, $check_query5);
+  
+  while($row=mysqli_fetch_array($result5))
+ 		{
+      $id = (int)$row['UserID'];
+      $user_check_query = "SELECT Alias FROM User WHERE UserID=$id LIMIT 1";
+      $new_result = mysqli_query($db, $user_check_query);
+      $user = mysqli_fetch_assoc($new_result);
+      
+ 			$json[$i] = array(
+        'id'=>$row['ID'],
+        'nickname'=>$user["Alias"],
+        'Topic' =>$row['Topic'],
+        'rating_1' =>$row['Rating_1'],
+        'rating_2' =>$row['Rating_2'],
+        'rating_3' =>$row['Rating_3'],
+        'rating_4' =>$row['Rating_4'],
+        'comment'=>$row['Comment'],
+        'date' => $row['Date']
+      );
+             $i = $i + 1;
+ 		} 
+    
   }
     
     
