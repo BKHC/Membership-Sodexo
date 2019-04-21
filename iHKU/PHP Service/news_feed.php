4feb0463123b412ac128e1ead49ab755a42ea5b9
Gavin1086 <34149012+Gavin1086@users.noreply.github.com>
2019-04-21 19:26:06 +0800

    Update news_feed.php

--- iHKU/PHP Service/news_feed.php
+++ iHKU/PHP Service/news_feed.php
@@ -92,10 +92,58 @@ session_start();
   {
    $search_hist = mysqli_fetch_array($hist_result);
     
-  $check_query = "SELECT * FROM `Hall Rate` ORDER BY '$search_hist' DESC UNION SELECT * FROM `Restaurant Rate` ORDER BY "$search_hist" DESC UNION SELECT * FROM `Course Rate` ORDER BY '$search_hist' DESC";
+  $check_query = "SELECT * FROM `Hall Rate` ORDER BY '$search_hist' DESC";
   $result = mysqli_query($db, $check_query);
   
   while($row=mysqli_fetch_array($result))
+ 		{
+      $id = (int)$row['UserID'];
+      $user_check_query = "SELECT Alias FROM User WHERE UserID=$id LIMIT 1";
+      $new_result = mysqli_query($db, $user_check_query);
+      $user = mysqli_fetch_assoc($new_result);
+      
+ 			$json[$i] = array(
+        'id'=>$row['ID'],
+        'nickname'=>$user["Alias"],
+        'Topic' =>$row['Topic'],
+        'rating_1' =>$row['Rating_1'],
+        'rating_2' =>$row['Rating_2'],
+        'rating_3' =>$row['Rating_3'],
+        'rating_4' =>$row['Rating_4'],
+        'comment'=>$row['Comment'],
+        'date' => $row['Date']
+      );
+             $i = $i + 1;
+ 		}
+    
+    $check_query1 = "SELECT * FROM `Restaurant Rate` ORDER BY "$search_hist" DESC";
+  $result1 = mysqli_query($db, $check_query1);
+  
+  while($row=mysqli_fetch_array($result1))
+ 		{
+      $id = (int)$row['UserID'];
+      $user_check_query = "SELECT Alias FROM User WHERE UserID=$id LIMIT 1";
+      $new_result = mysqli_query($db, $user_check_query);
+      $user = mysqli_fetch_assoc($new_result);
+      
+ 			$json[$i] = array(
+        'id'=>$row['ID'],
+        'nickname'=>$user["Alias"],
+        'Topic' =>$row['Topic'],
+        'rating_1' =>$row['Rating_1'],
+        'rating_2' =>$row['Rating_2'],
+        'rating_3' =>$row['Rating_3'],
+        'rating_4' =>$row['Rating_4'],
+        'comment'=>$row['Comment'],
+        'date' => $row['Date']
+      );
+             $i = $i + 1;
+ 		}
+    
+    $check_query2 = "SELECT * FROM `Course Rate` ORDER BY '$search_hist' DESC";
+  $result2 = mysqli_query($db, $check_query2);
+  
+  while($row=mysqli_fetch_array($result2))
  		{
       $id = (int)$row['UserID'];
       $user_check_query = "SELECT Alias FROM User WHERE UserID=$id LIMIT 1";
