b2d206602b0d99eb9a62e4ff7b9663e18e40270e
Gavin1086 <34149012+Gavin1086@users.noreply.github.com>
2019-04-21 18:50:25 +0800

    Update news_feed.php

--- iHKU/PHP Service/news_feed.php
+++ iHKU/PHP Service/news_feed.php
@@ -13,7 +13,7 @@ session_start();
   
   if($count == 0)
   {
-    $check_query = "SELECT * FROM `Hall Rate` ORDER BY rand() UNION SELECT * FROM `Restaurant Rate` ORDER BY rand() UNION SELECT * FROM `Course Rate` ORDER BY rand()";
+    $check_query = "SELECT * FROM `Hall Rate` ORDER BY rand()";
     $result = mysqli_query($db, $check_query);
   
   while($row=mysqli_fetch_array($result))
@@ -35,7 +35,57 @@ session_start();
         'date' => $row['Date']
       );
              $i = $i + 1;
+ 		} 
+    
+    $check_query1 = "SELECT * FROM `Restaurant Rate` ORDER BY rand()";
+    $result1 = mysqli_query($db, $check_query1);
+  
+  while($row=mysqli_fetch_array($result1))
+ 		{
+      $id = (int)$row['UserID'];
+      $user_check_query = "SELECT Alias FROM User WHERE UserID=$id LIMIT 1";
+      $new_result = mysqli_query($db, $user_check_query);
+      $user = mysqli_fetch_assoc($new_result);
+      
+ 			$json[$i] = array(
+        'id'=>$row['ID'],
+        'nickname'=>$user["Alias"],
+        'Topic' =>$row['Topic'],
+        'rating_1' =>$row['Rating_1'],
+        'rating_2' =>$row['Rating_2'],
+        'rating_3' =>$row['Rating_3'],
+        'rating_4' =>$row['Rating_4'],
+        'comment'=>$row['Comment'],
+        'date' => $row['Date']
+      );
+             $i = $i + 1;
  		}
+    
+    $check_query2 = "SELECT * FROM `Course Rate` ORDER BY rand()";
+    $result2 = mysqli_query($db, $check_query2);
+  
+  while($row=mysqli_fetch_array($result2))
+ 		{
+      $id = (int)$row['UserID'];
+      $user_check_query = "SELECT Alias FROM User WHERE UserID=$id LIMIT 1";
+      $new_result = mysqli_query($db, $user_check_query);
+      $user = mysqli_fetch_assoc($new_result);
+      
+ 			$json[$i] = array(
+        'id'=>$row['ID'],
+        'nickname'=>$user["Alias"],
+        'Topic' =>$row['Topic'],
+        'rating_1' =>$row['Rating_1'],
+        'rating_2' =>$row['Rating_2'],
+        'rating_3' =>$row['Rating_3'],
+        'rating_4' =>$row['Rating_4'],
+        'comment'=>$row['Comment'],
+        'date' => $row['Date']
+      );
+             $i = $i + 1;
+ 		}
+    
+    shuffle($json);
   }
 
   else
